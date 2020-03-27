a50cb3f89490016d101c97de36fe591f9e5cd1a1
Sonali-3010 <sonali.rathod30@gmail.com>
2020-03-27 11:33:13 +0530

    Modified App.js and index.css

--- App/App.js
+++ App/App.js
@@ -3,7 +3,10 @@ import React from 'react'
 class App extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div>
+                <h1>Hello!!</h1>
+                <h2>From React App</h2>
+            </div>
         )
     }
 }
